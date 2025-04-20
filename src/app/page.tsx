4b068a5cbeb24ad64c6f6e7873a388b7a85bec6a
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2025-04-20 19:42:53 +0100

    Add new components, updated page layout, configured image URL in Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import AudioPlayer from "@/components/audio-player";
+import ProgrammeMetadata from "@/components/programme-metadata";
+import RadioStationsList from "@/components/radio-stations-list";
+
 export default function Home() {
-  return <h1>Dash Audio Player</h1>
+  return (
+    <div className="bg-gray-100 p-4 flex justify-center items-center h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-md w-80">
+        <ProgrammeMetadata />
+
+        <AudioPlayer />
+
+        <RadioStationsList />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/programme-metadata.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+const IMAGE_URL = "https://ichef.bbci.co.uk/images/ic/160x160/{ID}.jpg.webp";
+
+export default function ProgrammeMetadata({
+  id,
+  title,
+  description,
+}: Partial<{
+  id: string;
+  title: string;
+  description: string;
+}>) {
+  const image = id ? (
+    <Image
+      width={320}
+      height={180}
+      src={IMAGE_URL.replace("{ID}", id)}
+      alt={`${title} - ${description}`}
+      className="w-64 h-64 mx-auto rounded-lg mb-4 shadow-lg shadow-teal-50"
+    />
+  ) : (
+    <Image
+      width={320}
+      height={180}
+      src="/globe.svg"
+      alt={"Select a radio station"}
+      className="w-64 h-64 mx-auto rounded-lg mb-4 shadow-lg shadow-teal-50"
+    />
+  );
+
+  const metadata =
+    id && title && description ? (
+      <>
+        <h2 className="text-xl font-semibold text-center">{title}</h2>
+        <p className="text-gray-600 text-sm text-center">{description}</p>
+      </>
+    ) : null;
+
+  return (
+    <>
+      {image}
+      {metadata}
+    </>
+  );
+}
